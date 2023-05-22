3f32ffa59e3d70b071dd2b4fb28aa3f68031eb94
Glitj Katj <glitjkatj@gmail.com>
2023-05-22 13:58:38 -0500

    Edit ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# This is a comment, apparently
+# Anything after an octothorpe is ignored by python
+
+print("This line is not behind an # and will not be ignored. It will be printed")
+
+# I can use the octothorpe to comment out a  line of code
+# print("This line won't run")
+
+print("This line will run")
