00efd079d6d3ec0fac633aa809469b7e661abd56
Glitj Katj <glitjkatj@gmail.com>
2023-05-22 14:43:35 -0500

    Edit ex4.py

---
+++ ex4.py
@@ -0,0 +1,19 @@
+# I decided to make this about ufos instead
+# I mean, why wouldn't aliens have a transportation corp?
+ufos = 100
+space_in_a_ufo = 4.0
+pilots = 30
+crew = 90
+ufos_not_driven = ufos - pilots
+ufos_piloted = pilots
+ufopool_capacity = ufos_piloted * space_in_a_ufo
+average_crew_per_ufo = crew / ufos_piloted
+
+print("There are ", ufos, " ufos available")
+print("There are only ", pilots, " pilots available")
+print("There will be ", ufos_not_piloted, " empty ufos today")
+print("We can transport ", ufopool_capacity, " crews today")
+print("We have ", crew, " to transport today")
+
+# For the next line, I'm gonna leave out the spacebar between the quotes to see what happens
+print("We need to put about", average_crew_per_ufo, "in each ufo")
