7a7507032d7bee23cffaf9cdeeb35f9678141bf8
Glitj Katj <glitjkatj@gmail.com>
2023-05-22 14:20:22 -0500

    Edit ex3.py

--- ex3.py
+++ ex3.py
@@ -6,6 +6,7 @@ print("Poison types", 2 * 15)
 print("Now I will count the fighting type pokemon")
 print(100 % 3)
 
+print("What happens if I try to print 4 + 2 < 7 - 5?")
 print(4 + 2 < 7 - 5)
 
 print("What's 4 + 2?", 4 + 2)
