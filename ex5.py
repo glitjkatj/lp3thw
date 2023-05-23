a720a90ef61a8cb15c6b95284570c77de8e81896
Glitj Katj <glitjkatj@gmail.com>
2023-05-23 12:03:01 -0500

    Edit ex5.py

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Thanos'
+my_age = 1000
+my_height = 79 #inches
+my_weight = 985 #lbs
+my_eyes = 'purple'
+my_teeth = 'white'
+my_hair = 'gray'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("That's crazy heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} unless Tony's snapped his fingers")
+
+# apparently this is line is tricky
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight}, I get {total}. But I don't know why I'm doing the addition to begin with.")
