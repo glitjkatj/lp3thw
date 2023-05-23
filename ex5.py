cfc255f176d79bba737d52a798fd4bffb5f88d8f
Glitj Katj <glitjkatj@gmail.com>
2023-05-23 13:00:05 -0500

    Add new variables for height and weight in metric in ex5.py

--- ex5.py
+++ ex5.py
@@ -1,15 +1,18 @@
 my_name = 'Thanos'
 my_age = 1000
-my_height = 79 #inches
-my_weight = 985 #lbs
+my_height_in_inches = 79 #inches
+my_height_in_cm = my_height_in_inches * 2.54
+my_weight_in_lbs = 985 #lbs
+my_weight_in_kg = my_weight_in_lbs * 0.45
 my_eyes = 'purple'
 my_teeth = 'white'
 my_hair = 'gray'
 
 print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
-print("That's crazy heavy.")
+print(f"He's {my_height_in_inches} inches tall.")
+print(f"That's {my_height_in_cm} in centimeters")
+print(f"He's {my_weight_in_lbs} pounds heavy.")
+print("That's about {my_weight_in_kg} in kilograms, and crazy heavy whichever unit you use.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} unless Tony's snapped his fingers")
 
