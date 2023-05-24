1f2bbcae29e636f9d022924d69cb298c5c74df2c
Glitj Katj <glitjkatj@gmail.com>
2023-05-24 14:17:37 -0500

    Edit ex7.py

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print("Row " * 3 + "...your boat")
+print("Gently down the {}.".format("stream"))
+print("Merrily " * 4)
+print("... life is but a dream.")
+print("*" * 10)
+
+end1 = "B"
+end2 = "e"
+end3 = "e"
+end4 = "f"
+end5 = "B"
+end6 = "u"
+end7 = "r"
+end8 = "r"
+end9 = "i"
+end10 = "t"
+end11 = "0"
+
+print(end1 + end2 + end3 + end4, end=' ')
+print(end5 + end6 + end7 + end8 + end9 + end11)
+
+print(end1 + end2 + end3 + end4)
+print(end5 + end6 + end7 + end8 + end9 + end11)
