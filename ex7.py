e7970b3492797db28884fbec0859f18aa9d2edfe
Glitj Katj <glitjkatj@gmail.com>
2023-05-24 14:25:35 -0500

    Add comments to certain lines to explain what the code does

--- ex7.py
+++ ex7.py
@@ -1,8 +1,8 @@
-print("Row " * 3 + "...your boat")
-print("Gently down the {}.".format("stream"))
-print("Merrily " * 4)
+print("Row " * 3 + "...your boat") # this prints "Row " three times
+print("Gently down the {}.".format("stream")) # this puts "stream" in the curly brackets
+print("Merrily " * 4) # this prints "Merrily " four times
 print("... life is but a dream.")
-print("*" * 10)
+print("*" * 10) # this prints "*" ten times
 
 end1 = "B"
 end2 = "e"
@@ -16,7 +16,7 @@ end9 = "i"
 end10 = "t"
 end11 = "o"
 
-print(end1 + end2 + end3 + end4, end=' ')
+print(end1 + end2 + end3 + end4, end=' ') # putting end=' ' after that comma puts a space in between the two prints, but not a line break
 print(end5 + end6 + end7 + end8 + end9 + end10 + end11)
 
 print(end1 + end2 + end3 + end4)
