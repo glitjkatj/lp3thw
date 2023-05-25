c5252f59833b4934f5e9709979078855654576b3
Glitj Katj <glitjkatj@gmail.com>
2023-05-25 14:26:20 -0500

    Edit ex9.py

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's some new(ish) weird stuff
+
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+print("""
+'Look on my Works, ye Mighty, and despair!'
+Nothing beside remains. Round the decay
+Of that colossal Wreck, boundless and bare
+The lone and level sands stretch far away.
+""")
