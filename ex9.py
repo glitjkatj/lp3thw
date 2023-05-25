347184ce0dbcc14bd9a4aac42aa4e2711df956f1
Glitj Katj <glitjkatj@gmail.com>
2023-05-25 14:31:34 -0500

    Add comments in lines 4 and 9 to explain code

--- ex9.py
+++ ex9.py
@@ -1,11 +1,12 @@
 # Here's some new(ish) weird stuff
 
 days = "Mon Tues Wed Thurs Fri Sat Sun"
-months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # \n is break line
 
 print("Here are the days:", days)
 print("Here are the months:", months)
 
+# multi line string with three quotes """
 print("""
 'Look on my Works, ye Mighty, and despair!'
 Nothing beside remains. Round the decay
