e6dd4ce70d44a05ba4b1ceeb227a5668f7564aa5
Glitj Katj <glitjkatj@gmail.com>
2023-05-25 14:43:38 -0500

    Edit ex10.py

---
+++ ex10.py
@@ -0,0 +1,15 @@
+apa_cat = "\tI'm a cat that indent the first line of each new paragraph"
+yoga_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I have a chore for you, human:
+\t* Fancy salmon
+\t* Temptations shrimpies
+\t* Catnip \n\t*Kush
+"""
+
+print(apa_cat)
+print(yoga_cat)
+print(backslash_cat)
+print(fat_cat)
