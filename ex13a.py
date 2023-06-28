6b4e3dc40902ead296568fd0ac8d46c3e5e854b4
Glitj Katj <glitjkatj@gmail.com>
2023-06-28 10:39:03 -0500

    Add code to ex13a.py

---
+++ ex13a.py
@@ -0,0 +1,7 @@
+from sys import argv
+input_arg = input("Type something, bro ")
+script, cm_arg = argv
+
+print("So the script's name is ", script)
+print("The command line argument is ", cm_arg)
+print("The argument from input is ", input_arg)
